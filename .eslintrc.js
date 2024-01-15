ce22b0713dfc9528e1cbab8a33893b067cc4dd14
BrayhanVargas <balbervq@gmail.com>
2024-01-15 17:48:24 -0500

    first version with linter y prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'no-unused-vars': 'error',
+    'comma-dangle': ['error', 'never']
+  }
+};
